4c4b6961d4abfd5511a54779642ff6e962e461d8
ADMIN <ADMIN@192.168.0.24>
2017-06-05 19:14:10 +0530

    web app  BasicCalc.java-B.swetha

--- src/calculator/BasicCalc.java
+++ src/calculator/BasicCalc.java
@@ -8,5 +8,44 @@ public class BasicCalc
 	//include getters and setters method to access variables
 	
 	//include methods for operations
+	double a,b,c;
+	
+		public double getA() {
+			
+		return a;
+	}
+	public void setA(double a) {
+		this.a = a;
+	}
+	public double getB() {
+		
+		return b;
+	}
+	public void setB(double b) {
+		
+		this.b = b;
+	}
+	
+	
+	double Add()
+	{
+		c=a+b;
+		return(c);
+	}
+	double Subtract()
+	{
+		c=a-b;
+		return(c);
+	}
+	double Multiply()
+	{
+		c=a*b;
+		return(c);
+	}
+	double Divide()
+	{
+		c=a/b;
+		return(c);
+	}
 	
 }
